57ce70da95da11a1e5fc02968bc6f17f73ca5990
Ch1fer <evgengajdas@gmail.com>
2025-03-04 16:24:06 +0100

    fix imports

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify, make_response
-from flask_sqlaclchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     __tablename__ = 'users'
